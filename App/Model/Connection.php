f724e4bac2409005494e19090a225b457eda2794
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-07 20:00:05 -0300

    refactor: Connection using PDO + Singleton

---
+++ App/Model/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+class Connection
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new \PDO("sqlite:db.sqlite");
+            self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
