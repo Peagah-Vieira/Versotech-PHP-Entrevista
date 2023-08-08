4406cf5c0d415995539331604ad7d1487e439d2f
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-08 01:07:57 -0300

    feat: Register - data persistency

--- register.php
+++ register.php
@@ -2,11 +2,23 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-if (isset($_POST['name'], $_POST['email'])) {
-    die('Cadastrar');
-}
-
 define('TITLE', 'Cadastrar Usuário');
+
+use \App\Models\User;
+use \App\Models\UserDao;
+
+// POST VALIDATION
+if (isset($_POST['name'], $_POST['email'])) {
+    $user = new User();
+    $userDao = new UserDao();
+
+    $user->setName($_POST['name']);
+    $user->setEmail($_POST['email']);
+
+    $userDao->create($user);
+
+    echo $_POST['name'] . "was created successfully";
+}
 
 include __DIR__ . '/includes/header.php';
 include __DIR__ . '/includes/form.php';
